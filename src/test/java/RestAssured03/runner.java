80d31f3c8524f8beb88d4fc59c3a700ed4c83a64
abhishektaware09 <abhitaware91@gmail.com>
2020-11-03 00:17:46 +0530

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ src/test/java/RestAssured03/runner.java
@@ -0,0 +1,11 @@
+package RestAssured03;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = true)
+public class runner {
+	
+}
